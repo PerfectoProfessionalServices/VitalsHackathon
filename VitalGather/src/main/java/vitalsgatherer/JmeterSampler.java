4bd5005f82363b68c9234ca02b989afe1c2b781c
jeremy payne <jeremyp@perfectomobile.com>
2016-09-16 19:48:57 -0400

    updated jmeter sampler to latest vital sample updates

--- VitalGather/src/main/java/vitalsgatherer/JmeterSampler.java
+++ VitalGather/src/main/java/vitalsgatherer/JmeterSampler.java
@@ -51,38 +51,47 @@ public class JmeterSampler extends AbstractJavaSamplerClient implements Serializ
 					e.printStackTrace();
 				}
 
-				String url = "http://localhost:8083/Birt/run?__report=vitals.rptdesign&format=html&__title=Performance+Test+Vitals+Gather+"
-						+ URLEncoder.encode(startDate.toString(), "UTF-8");
+				String url = "http://localhost:8083/WebViewerExample/run?__report=vitals.rptdesign&format=html&__title=Performance+Test+Vitals+Gather+"
+						+ URLEncoder.encode(dt.toString(), "UTF-8");
 
 				File vfandroid = null;
 				File vfios = null;
-				if (results.get(VitalsGather.resultOptions.statusAndroid)
-						.equals("success")) {
+				boolean foundSuccess = false;
+				if (results.get(VitalsGather.resultOptions.statusAndroid).equals("success")) {
 					System.out.println(results.get(VitalsGather.resultOptions.jsonAndroid));
 					vfandroid = new File(results.get(VitalsGather.resultOptions.fullPathCsvAndroid));
 					url = url + "&AndroidCSVFile=" + URLEncoder.encode(vfandroid.getAbsolutePath(), "UTF-8");
+					foundSuccess = true;
 				}
-				if (results.get(VitalsGather.resultOptions.statusIos)
-						.equals("success")) {
+				if (results.get(VitalsGather.resultOptions.statusIos).equals("success")) {
 					System.out.println(results.get(VitalsGather.resultOptions.jsonIos));
 					vfios = new File(results.get(VitalsGather.resultOptions.fullPathCsvIos));
 					url = url + "&iOSCSVFile=" + URLEncoder.encode(vfios.getAbsolutePath(), "UTF-8");
+					foundSuccess = true;
 				}
 
-				if (results.get(VitalsGather.resultOptions.statusTransactionAndroid)
-						.equals("success")) {
+				if (results.get(VitalsGather.resultOptions.statusTransactionAndroid).equals("success")) {
 					System.out.println(results.get(VitalsGather.resultOptions.jsonTransactionAndroid));
 					vfandroid = new File(results.get(VitalsGather.resultOptions.fullPathTransactionCsvAndroid));
 					url = url + "&AndroidTransactionCSVFile=" + URLEncoder.encode(vfandroid.getAbsolutePath(), "UTF-8");
+					foundSuccess = true;
 				}
-				if (results.get(VitalsGather.resultOptions.statusTransactionIos)
-						.equals("success")) {
+				if (results.get(VitalsGather.resultOptions.statusTransactionIos).equals("success")) {
 					System.out.println(results.get(VitalsGather.resultOptions.jsonTransactionIos));
 					vfios = new File(results.get(VitalsGather.resultOptions.fullPathTransactionCsvIos));
 					url = url + "&iOSTransactionCSVFile=" + URLEncoder.encode(vfios.getAbsolutePath(), "UTF-8");
+					foundSuccess = true;
 				}
 
-				Desktop.getDesktop().browse(new URI(url));
+				if (foundSuccess) {
+					try {
+						Thread.sleep(5000);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					Desktop.getDesktop().browse(new URI(url));
+				}
 
 				result.sampleEnd(); // stop stopwatch
 				result.setSuccessful( true );
